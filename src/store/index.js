9ce5a9b262cf5cf4084eead64279f6dbfb524d9b
Mairamkul <mairamkulormonova@gmail.com>
2023-02-19 23:18:19 +0600

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {combineReducers,applyMiddleware, createStore} from 'redux'
+import  { mealsSlice}  from './meals/MealsSlice'
+import thunk from 'redux-thunk'
+import {  basketSlice } from './meals/BasketSlice'
+
+const rootReducer = combineReducers({
+    [mealsSlice.name]: mealsSlice.reducer,
+    [basketSlice.name]: basketSlice.reducer
+
+})
+
+export const store = createStore(rootReducer,applyMiddleware(thunk))
